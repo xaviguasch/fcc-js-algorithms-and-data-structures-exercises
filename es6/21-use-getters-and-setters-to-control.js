13b3f2e32677f3096d1cdbee662b9d774d7906f2
xaviguasch <xaviguasch@gmail.com>
2020-06-08 23:10:44 +0200

    added solution for ES6 exercise 21: Use getters and setters to control access to an object

---
+++ es6/21-use-getters-and-setters-to-control.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+class Thermostat {
+  constructor(F) {
+    this._F = F
+  }
+  get temperature() {
+    const C = (5 / 9) * (this._F - 32)
+    return C
+  }
+
+  set temperature(C) {
+    this._F = (C * 9.0) / 5 + 32
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76) // Setting in Fahrenheit scale
+let temp = thermos.temperature // 24.44 in Celsius
+thermos.temperature = 26
+temp = thermos.temperature // 26 in Celsius
