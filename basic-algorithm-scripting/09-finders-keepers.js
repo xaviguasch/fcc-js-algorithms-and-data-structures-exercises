78bbefdaaf68823dcb9e54c265d3e4a356fe845e
xaviguasch <xaviguasch@gmail.com>
2020-06-15 17:20:52 +0200

    added another refactored solution to BAS ex9

--- basic-algorithm-scripting/09-finders-keepers.js
+++ basic-algorithm-scripting/09-finders-keepers.js
@@ -1,12 +1,14 @@
 function findElement(arr, func) {
-  let num = 0
-  for (let i = 0; i < arr.length; i++) {
-    num = arr[i]
-    if (func(num)) {
-      return num
-    }
-  }
-  return undefined
+  // let num = 0
+  // for (let i = 0; i < arr.length; i++) {
+  //   num = arr[i]
+  //   if (func(num)) {
+  //     return num
+  //   }
+  // }
+  // return undefined
+
+  return arr.find(func)
 }
 
 console.log(findElement([1, 2, 3, 4], (num) => num % 2 === 0))
