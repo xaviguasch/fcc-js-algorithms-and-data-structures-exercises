a4ac65ef271e40985b8ed46465035eebd1e1a7ce
xaviguasch <xaviguasch@gmail.com>
2020-06-15 16:57:24 +0200

    added solution to BAS ex8: Truncate a string

---
+++ basic-algorithm-scripting/08-truncate-a-string.js
@@ -0,0 +1,17 @@
+function truncateString(str, num) {
+  console.log(num)
+  if (str.length > num) {
+    return str.slice(0, num) + '...'
+  } else {
+    return str
+  }
+}
+
+console.log(truncateString('A-tisket a-tasket A green and yellow basket', 8))
+
+// console.log(
+//   truncateString(
+//     'A-tisket a-tasket A green and yellow basket',
+//     'A-tisket a-tasket A green and yellow basket'.length + 2
+//   )
+// )
