0737fdabbd1213d0ddd48b01a717ce95ba8ad7f4
xaviguasch <xaviguasch@gmail.com>
2020-06-19 14:05:29 +0200

    added solution for FP ex 18: Return a Sorted Array Without Changing the Original Array

---
+++ functional-programming/18-return-a-sorted-array-without-changing.js
@@ -0,0 +1,10 @@
+var globalArray = [5, 6, 3, 2, 9, 11, 0]
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  let newArr = [...arr]
+
+  return newArr.sort((a, b) => a - b)
+  // Only change code above this line
+}
+
+console.log(nonMutatingSort(globalArray))
