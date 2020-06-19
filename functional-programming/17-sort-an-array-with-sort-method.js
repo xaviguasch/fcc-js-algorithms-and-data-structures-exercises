db41add68a76721420f0ec864aa7fd401b01bd6c
xaviguasch <xaviguasch@gmail.com>
2020-06-19 13:49:23 +0200

    solved error in previous solution for FP 17

--- functional-programming/17-sort-an-array-with-sort-method.js
+++ functional-programming/17-sort-an-array-with-sort-method.js
@@ -1,7 +1,7 @@
 function alphabeticalOrder(arr) {
   // Only change code below this line
 
-  return arr.sort((a, b) => a - b)
+  return arr.sort((a, b) => (a === b ? 0 : a > b ? 1 : -1))
   // Only change code above this line
 }
 
